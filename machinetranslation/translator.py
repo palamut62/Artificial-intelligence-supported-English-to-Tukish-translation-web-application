4bf7cb652849d19bb85f354a1c541fece4007fe6
umutins62 <umutins62@hotmail.com>
2023-09-06 08:49:09 +0300

    update:index.html

--- machinetranslation/translator.py
+++ machinetranslation/translator.py
@@ -1,5 +1,5 @@
 from deep_translator import MyMemoryTranslator
-
+from gtts import gTTS
 def english_to_french(english_text):
     """
     Function to translate English text to French.
@@ -23,3 +23,25 @@ def french_to_english(french_text):
     str: Translated text in English
     """
     return MyMemoryTranslator(source='turkish', target='english').translate(french_text)
+def text_to_speech(text):
+    """
+    Function to translate French text to English.
+
+    Parameters:
+    french_text (str): Text in French to be translated to English
+
+    Returns:
+    str: Translated text in English
+    """
+
+    # Seslendirmek istediğin metin
+    metin = text
+
+    # Metni Türkçe olarak seslendir
+    tts = gTTS(text=metin, lang='tr')
+
+    # Ses dosyasını kaydet
+    tts.save("merhaba.mp3")
+
+    # Ses dosyasını oynat (Linux ve MacOS için)
+    os.system("start merhaba.mp3")
